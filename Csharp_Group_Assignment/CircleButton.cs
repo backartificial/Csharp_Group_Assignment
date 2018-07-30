36c9967b71133490f05284c9029c579d5e211afc
James Grau <backartificial@users.noreply.github.com>
2018-07-29 21:46:15 -0400

    Renamed CircleImage to CircleButton
    - Renamed CircleImage to CircleButton

--- Csharp_Group_Assignment/CircleButton.cs
+++ Csharp_Group_Assignment/CircleButton.cs
@@ -3,7 +3,9 @@ using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace Csharp_Group_Assignment {
-    class CircleImage: Button {
+
+    // Taken from https://www.youtube.com/watch?v=U6cil0Gtb44
+    class CircleButton : Button {
         protected override void OnPaint(PaintEventArgs pevent) {
             GraphicsPath grPath = new GraphicsPath();
             grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
